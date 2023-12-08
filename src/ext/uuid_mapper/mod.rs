e9c3e5f09dccbdb2f5f3fdd6034ff8f744de560f
Kirill Fertikov <kirill.fertikov@gmail.com>
2023-12-08 23:17:06 +0700

    minor

--- src/ext/uuid_mapper/mod.rs
+++ src/ext/uuid_mapper/mod.rs
@@ -13,7 +13,10 @@ mod redis;
 #[cfg(feature = "redis-storage")]
 const REDIS_URL_ENV: &str = "REDIS_URL";
 
-pub fn create<V: Clone + Sync + Send + Serialize + DeserializeOwned + 'static>() -> Box<dyn UuidMapper<V>> {
+pub fn create<
+    #[cfg(feature = "redis-storage")] V: Clone + Sync + Send + Serialize + DeserializeOwned + 'static,
+    #[cfg(not(feature = "redis-storage"))] V: Clone + Sync + Send + 'static,
+>() -> Box<dyn UuidMapper<V>> {
     #[cfg(feature = "redis-storage")]
     if let Ok(redis_url) = std::env::var(REDIS_URL_ENV) {
         return Box::new(RedisUuidMapper::new(&redis_url)
